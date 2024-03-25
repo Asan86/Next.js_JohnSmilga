d6b7442c3cb5629705620bf95939d554bb74e8a9
Asan Turdukulov <asanturdukulov86@gmail.com>
2024-03-25 22:42:16 +0600

    Loading Component

--- src/app/drinks/page.js
+++ src/app/drinks/page.js
@@ -1,8 +1,16 @@
+import { resolve } from "path";
 import React from "react";
 const url = "https://jsonplaceholder.typicode.com/users";
-const DrinksPage = async () => {
+
+const fetchDrinks = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const respons = await fetch(url);
   const data = await respons.json();
+  return data;
+};
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
   console.log(data);
   return (
     <div>
